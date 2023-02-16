d980d936acfd3040a9c357347f1ddae1cf22d396
Bruno Campos <campos.bruno98@gmail.com>
2023-02-16 15:42:02 -0300

    initial commit

---
+++ js/camera.js
@@ -0,0 +1,38 @@
+const btnIniciarCamera = document.querySelector('[data-video-botao class]');
+const campoCamera = document.querySelector('[data-camera]');
+const video = document.querySelector('[data-video]');
+const botaoTirarFoto = document.querySelector('[data-tirar-foto]');
+const canvas = document.querySelector('[data-video-canvas]');
+const mensagem = document.querySelector('[data-mensagem]');
+const botaoEnviarFoto = document.querySelector('[data-enviar]');
+
+const imagemURL = '';
+
+btnIniciarCamera.addEventListener('click', async function () {
+    const iniciarVideo = await navigator.mediaDevices
+        .getUserMedia({ video: true, audio: false })
+
+    btnIniciarCamera.style.display = 'none'
+
+    video.srcObject = iniciarVideo;
+})
+
+botaoTirarFoto.addEventListener('click', function () {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.heigth);
+
+    imagemURL = canvas.toDataURL('image/jpg');
+
+    campoCamera.style.display = 'none';
+    mensagem.style.display = 'block'
+})
+
+botaoEnviarFoto.addEventListener('click', () => {
+    const receberDadosExistentes = localStorage.getItem('cadastro')
+    const converteRetorno = JSON.parse(receberDadosExistentes)
+
+    converteRetorno.imagem = imagemURL;
+
+    localStorage.setItem('cadastro', JSON.stringify(converteRetorno));
+
+    window.location.href = './abrir-conta-form-3.html';
+})
